9e59e65bb0360c752861b8a61e0b091d6a686564
apaulraj <anbarasan.paulraj@arris.com>
2019-02-15 20:04:19 +0530

    Group message implemented

--- app/src/main/java/com/nec/staffApp/SearchActivity.java
+++ app/src/main/java/com/nec/staffApp/SearchActivity.java
@@ -1,6 +1,10 @@
 package com.nec.staffApp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,7 +18,6 @@ import com.nec.staffApp.multichoice.SampleCustomViewAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import butterknife.ButterKnife;
 
@@ -45,6 +48,39 @@ public class SearchActivity extends AppCompatActivity {
                 List<Integer> selectedList = adapter.getSelectedItemList();
                 List<Staff> selectedStaff = getSelectedStaff(selectedList);
                 Toast.makeText(SearchActivity.this, selectedStaff.size()+"", Toast.LENGTH_SHORT).show();
+
+                if(selectedStaff != null && selectedStaff.size() > 0){
+                    String numbers[] = new String[selectedStaff.size()];
+                    for (int i = 0; i < selectedStaff.size(); i++) {
+                        numbers[i] = selectedStaff.get(i).phno;
+                    }
+                    String num = "";
+                    for (int i = 0; i < numbers.length; i++) {
+                        num = num +";"+numbers[i];
+                    }
+
+                    Intent intent;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) // Android 4.4 and up
+                    {
+                        String defaultSmsPackageName = Telephony.Sms.getDefaultSmsPackage(getApplicationContext());
+
+                        intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + Uri.encode(num)));
+                        intent.putExtra("sms_body", "Hello");
+
+                        if (defaultSmsPackageName != null) // Can be null in case that there is no default, then the user would be able to choose any app that supports this intent.
+                        {
+                            intent.setPackage(defaultSmsPackageName);
+                        }
+                    }
+                    else
+                    {
+                        intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setType("vnd.android-dir/mms-sms");
+                        intent.putExtra("address", num.toString());
+                        intent.putExtra("sms_body", "hello");
+                    }
+                    startActivity(intent);
+                }
             }
         });
 
